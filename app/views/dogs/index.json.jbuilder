fd0d799fb7cfe91491d3222db75e5d3ad71f08be
Matthew Cowie <matthew@cowie.me>
2015-12-02 23:15:01 -0600

    Add age to Dogs

--- app/views/dogs/index.json.jbuilder
+++ app/views/dogs/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@dogs) do |dog|
-  json.extract! dog, :id, :name
+  json.extract! dog, :id, :name, :age
   json.url dog_url(dog, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203045321) do
+ActiveRecord::Schema.define(version: 20151203050927) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
