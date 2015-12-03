18dc5324d357e768ba42c18ab4ba59163d35590f
Matthew Cowie <matthew@cowie.me>
2015-12-02 23:24:25 -0600

    added pictures to dogs
    	modified:   Gemfile
    	modified:   Gemfile.lock
    	modified:   app/controllers/dogs_controller.rb
    	modified:   app/models/dog.rb
    	new file:   app/uploaders/picture_uploader.rb
    	new file:   config/cloudinary.yml
    	new file:   db/migrate/20151203052013_add_picture_to_dogs.rb
    	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203050927) do
+ActiveRecord::Schema.define(version: 20151203052013) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "age"
+    t.string   "picture"
   end
 
 end
